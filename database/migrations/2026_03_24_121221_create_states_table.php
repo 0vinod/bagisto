a64e63935e196ed713eb234e56d67d2a3fe16094
0vniod <vinod190596@gmail.com>
2026-03-24 18:13:34 +0530

    zXZx

---
+++ database/migrations/2026_03_24_121221_create_states_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('states', function (Blueprint $table) {
+            $table->id();
+            $table->string('country_name', 191);
+            $table->string('state_name', 191);
+            $table->bigInteger('country_id')->default(1);
+            $table->timestamps();
+        });
+
+        schema::create('cities', function (Blueprint $table) {
+            $table->id();
+            $table->string('state_name', 191);
+            $table->string('city_name', 191);
+            $table->bigInteger('state_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('states');
+    }
+};
